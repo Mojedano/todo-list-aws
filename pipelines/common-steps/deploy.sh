fa74393ffa5f9b3a636d2a698751ba3359fff84c
Ubuntu <ubuntu@ip-172-31-80-139.ec2.internal>
2023-05-26 10:10:40 +0000

    CAMBIO EN EL FICHERO deploy.sh. AÑADIDO AL COMANDO EL PARÁMETRO --region OBJETIVO CONSTRUCCIÓN CORRECTA PIPELINE-FULL-PRODUCTION

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --resolve-s3 --stack-name "todo-list-aws-production"
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --resolve-s3 --stack-name "todo-list-aws-production" --region "us-east-1"
