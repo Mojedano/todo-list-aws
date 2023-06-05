e727344f655099dfe686635f4e5ac190e6f88206
Ubuntu <ubuntu@ip-172-31-80-139.ec2.internal>
2023-06-05 17:45:54 +0000

    He resultado el problema del pipeline-full-staging pero en el merge falla. Doy marcha atras. vuelvo a modificar deploy.sh y samconfig.toml para dejarlo como estaba y que funcione todo ok

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -3,4 +3,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name "todo-list-aws-staging" --region "us-east-1"
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --resolve-s3
