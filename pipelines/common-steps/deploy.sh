63f1870c5b90191963acfe99ec8520f87fe5c76d
Ubuntu <ubuntu@ip-172-31-80-139.ec2.internal>
2023-06-05 17:10:05 +0000

    Ultimo intento de mejora. Añado al fichero deploy.sh --stack-name todo-list-aws staging --region us-east-1 y en el fichero samconfig.toml incluso linea S3_bucket a la seccion staging y production. Esta vez he añadido stanging, la anterior vez creo que no lo modifique y deje production y puede ser que por eso fallara. De verdad ultimo intento.

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -3,4 +3,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --resolve-s3
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --resolve-s3 --stack-name "todo-list-aws-staging" --region "us-east-1"
